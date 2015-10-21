eb199d6847c8e16dcebf771428d6df0ab99a7bf9
lfernandortiz <lfernandortiz@gmail.com>
2015-10-20 21:49:13 -0500

    Edicion clase StringUtil

--- src/com/luisfernandoortiz/commons/StringUtil.java
+++ src/com/luisfernandoortiz/commons/StringUtil.java
@@ -1,5 +1,38 @@
 package com.luisfernandoortiz.commons;
 
+/**
+ * @author Luis Fernando Ortiz Vera | @lfernandortiz
+ *         <p>
+ *         La clase <code>StringUtil</code> contiene metodos
+ *         <code>static</code> para manipular cadenas que generalmente
+ *         son campos de formularios, los cuales requieren ser validados
+ *         antes de ser persistidos en una base de datos. Tambien
+ *         contiene metodos para validar cadenas.
+ *         </p>
+ */
 public class StringUtil {
-
+	
+	
+	
+	/**
+	 * El metodo <tt>validateEmail </tt> retorna true si la direccion de correo 
+	 * electronico es una direccion valida. Este metodo usa expresiones regulares
+	 * (regex) para hacer el analisis.
+	 * 
+	 * http://docs.oracle.com/javase/tutorial/essential/regex/intro.html
+	 * 
+	 * @param mail
+	 * @return
+	 */
+	public static boolean validateEmail(String mail){
+		
+		if (!mail
+				.matches("\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*")) {
+			return false;
+		} else {
+			return true;
+		}		
+		
+	}
+	
 }
